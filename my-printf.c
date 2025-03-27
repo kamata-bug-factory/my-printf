3d0b934266a42f0a379f3592a140f2498d031fa2
kamata-bug-factory <kazukazuki517@gmail.com>
2025-03-27 10:32:32 +0900

    feat: UTF-8文字を1文字ずつ出力できるようにする

--- my-printf.c
+++ my-printf.c
@@ -1,6 +1,21 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+// UTF-8 文字を 1 文字ずつ出力
+void my_put_utf8_char(const char **p) {
+    const unsigned char *s = (const unsigned char *)(*p);
+    int bytes = 1;
+
+    // 先頭バイトからバイト数を判定
+    if ((*s & 0x80) == 0)        bytes = 1;  // ASCII (1バイト)
+    else if ((*s & 0xE0) == 0xC0) bytes = 2;  // 2バイト文字
+    else if ((*s & 0xF0) == 0xE0) bytes = 3;  // 3バイト文字
+    else if ((*s & 0xF8) == 0xF0) bytes = 4;  // 4バイト文字
+
+    write(1, s, bytes);  // 必要なバイト数だけ出力
+    *p += bytes;  // ポインタを進める
+}
+
 void my_printf(const char *format, ...) {
     va_list args;
     va_start(args, format);
@@ -18,8 +33,8 @@ void my_printf(const char *format, ...) {
                 default: write(1, "?", 1); break;
             }
         } else {
-            // TODO: UTF-8 文字を考慮したフォーマット指定子以外を出力できるようにする
-            write(1, format, 1);
+            my_put_utf8_char(&format);  // UTF-8 文字対応
+            continue;  // `my_put_utf8_char` で `format` を更新したので `format++` は不要
         }
         format++;
     }
@@ -28,6 +43,7 @@ void my_printf(const char *format, ...) {
 }
 
 int main() {
+    my_printf("日本語\n");
     my_printf("char: %c\n", 'A');
     my_printf("%%\n");
     my_printf("%a\n");
